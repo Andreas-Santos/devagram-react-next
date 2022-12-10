a1cd02d01c9981fddb32328f392a4b997954213e
Andreas-Santos <andreassantos.3003@gmail.com>
2022-12-09 23:36:32 -0300

    Validadores, componentes de botão e componentes de avatar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Avatar from '../componentes/avatar'
+import Botao from '../componentes/botao'
 
 export default function Home() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <div style={{width: 200}}>
+        <Avatar />
+        <Botao texto={'Login'} cor='invertido' manipularClique={() => console.log('botão clicado')} />
+      </div>
+    </>
   )
 }
