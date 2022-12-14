2b0c08eecc22757054399007156c84917cc9b115
Andreas-Santos <andreassantos.3003@gmail.com>
2022-12-13 21:47:59 -0300

    Estilização dos componentes do feed

--- componentes/layout/Cabecalho.js
+++ componentes/layout/Cabecalho.js
@@ -1,10 +1,49 @@
 import Image from 'next/image';
 import Navegacao from './Navegacao';
+import { useState } from 'react';
 
 import logoHorizontalImg from '../../public/imagens/logoHorizontal.svg';
 import imagemLupa from '../../public/imagens/lupa.svg';
+import ResultadoPesquisa from './ResultadoPesquisa';
 
 export default function Cabecalho() {
+    const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+    const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+    const aoPesquisar = (e) => {
+        setTermoPesquisado(e.target.value);
+        setResultadoPesquisa([]);
+
+        if (termoPesquisado.length < 3) {
+            return;
+        }
+
+        setResultadoPesquisa([
+            {
+                avatar: '',
+                nome: 'Douglas',
+                email: 'douglas@devagram.com',
+                _id: '123456'
+            },
+            {
+                avatar: '',
+                nome: 'Andreas',
+                email: 'andreas@gmail.com',
+                _id: '345678'
+            },
+            {
+                avatar: '',
+                nome: 'Nicolas',
+                email: 'nicolas@gmail.com',
+                _id: '731235'
+            }
+        ]);
+    }
+
+    const aoClicarResultadoPesquisa = (id) => {
+        console.log('aoClicarResultadoPesquisa', {id}); 
+    }
+
     return (
         <header className='cabecalhoPrincipal'>
             <div className='conteudoCabecalhoPrincipal'>
@@ -28,13 +67,28 @@ export default function Cabecalho() {
                     <input
                         type='text'
                         placeholder='Pesquisar'
-                        value={''}
-                        onChange={() => console.log('pesquisando')}
+                        value={termoPesquisado}
+                        onChange={aoPesquisar}
                     />
                 </div>
 
                 <Navegacao className='desktop' />
             </div>
+
+            {resultadoPesquisa.length > 0 && (
+                <div className='resultadoPesquisaContainer'>
+                    {resultadoPesquisa.map(r => (
+                        <ResultadoPesquisa
+                            avatar={r.avatar}
+                            nome={r.nome}
+                            email={r.email}
+                            key={r._id}
+                            id={r._id}
+                            onClick={aoClicarResultadoPesquisa}
+                        />
+                    ))}
+                </div>
+            )}
         </header>
     );
 }
